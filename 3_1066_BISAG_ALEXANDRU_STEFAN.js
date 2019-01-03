3056dde7373e158c3c88b12e47077afac4835776
axbg <bisagalexstefan@gmail.com>
2019-01-03 15:12:20 +0200

    Implemented CSS Grid.

--- 3_1066_BISAG_ALEXANDRU_STEFAN.js
+++ 3_1066_BISAG_ALEXANDRU_STEFAN.js
@@ -1,6 +1,21 @@
+let canvas;
+let graphContainer;
+let data;
 
 window.onload = function (event) {
-    
-    console.log('loaded');
 
+    graphContainer = document.querySelector('.graph-container');
+    canvas = document.querySelector('canvas');
+
+    setDimensions(canvas, graphContainer, 90, 20);
+    window.onresize = function (event) {
+        setDimensions(canvas, graphContainer, 90, 20);;
+    }
+
+
+}
+
+function setDimensions(destination, source, heightDifference, widthDifferene) {
+    destination.height = source.offsetHeight - heightDifference;
+    destination.width = source.offsetWidth - widthDifferene;
 }
