4ceeafb38264757c9cb3dd1bb324863a27bdb695
axbg <bisagalexstefan@gmail.com>
2019-01-05 13:53:27 +0200

    Implemented multiple years slide. Implemented persistent country focus. Implemented country active selection.

--- 3_1066_BISAG_ALEXANDRU_STEFAN.js
+++ 3_1066_BISAG_ALEXANDRU_STEFAN.js
@@ -1,38 +1,66 @@
 let canvas, context;
 let graphContainer;
-let dataString = '{"title":"A study of countries","firstParam":"Average of Women Fertility (children/woman)","secondParam":"Average Years Of Schooling","thirdParam":"$/capita","years":[{"year":1979,"countries":[{"name":"Romania","color":"#FF7F0E","firstParam":1.26,"secondParam":9.2,"thirdParam":39231.13},{"name":"UK","color":"#1F77B4","firstParam":1.37,"secondParam":11.2,"thirdParam":439231.13},{"name":"USA","color":"#9467BD","firstParam":2.37,"secondParam":11.2,"thirdParam":249231.13}]}]}'
+let dataString = '{"title":"A study of countries","firstParam":"Average of Women Fertility (children/woman)","secondParam":"Average Years Of Schooling","thirdParam":"$/capita","years":[{"year":1979,"countries":[{"name":"Romania","color":"#FF7F0E","firstParam":2.45,"secondParam":8.4,"thirdParam":3930},{"name":"UK","color":"#1F77B4","firstParam":1.72,"secondParam":10.4,"thirdParam":22300},{"name":"USA","color":"#9467BD","firstParam":1.8,"secondParam":12.1,"thirdParam":29100}]},{"year":1980,"countries":[{"name":"Romania","color":"#FF7F0E","firstParam":2.4,"secondParam":8.5,"thirdParam":4060},{"name":"UK","color":"#1F77B4","firstParam":1.73,"secondParam":10.5,"thirdParam":21900},{"name":"USA","color":"#9467BD","firstParam":1.82,"secondParam":12.2,"thirdParam":28700}]},{"year":1981,"countries":[{"name":"Romania","color":"#FF7F0E","firstParam":2.36,"secondParam":8.7,"thirdParam":4190},{"name":"UK","color":"#1F77B4","firstParam":1.74,"secondParam":10.6,"thirdParam":21700},{"name":"USA","color":"#9467BD","firstParam":1.81,"secondParam":12.3,"thirdParam":29200}]},{"year":1982,"countries":[{"name":"Romania","color":"#FF7F0E","firstParam":2.32,"secondParam":8.8,"thirdParam":4320},{"name":"UK","color":"#1F77B4","firstParam":1.76,"secondParam":10.8,"thirdParam":22100},{"name":"USA","color":"#9467BD","firstParam":1.81,"secondParam":12.4,"thirdParam":28400}]}]}'
 let data;
 let yMarkDistance, xMarkDistance, yValueDistance, xValueDistance;
 let origin, maxRound, countries;
+let minYear, maxYear;
+let currentCountry = null;
+let currentYear;
+let isPlaying = false;
+let canvasFullWidth = null;
 
 window.onload = function (event) {
 
     data = JSON.parse(dataString);
     graphContainer = document.querySelector('.graph-container');
     canvas = document.querySelector('canvas');
+    slider = document.querySelector('input[type="range"');
     context = canvas.getContext('2d');
 
+    calculateYearRange();
+    slider.setAttribute('max', maxYear);
+    slider.setAttribute('min', minYear);
+    slider.value = currentYear = minYear;
+
     setDimensions(canvas, graphContainer, 90, 20);
+    canvasFullWidth = canvas.width;
+
     window.onresize = function (event) {
+        clearCanvas();
         setDimensions(canvas, graphContainer, 90, 20);
-        initUI(1979);
+        scaleCanvasCoordinates();
+        updateUI(currentYear);
         drawAxis();
-        drawCircles(1979)
+        drawCircles(currentYear);
+        countryFocus(currentCountry);
     }
 
-    initUI(1979);
+    slider.addEventListener('input', function (e) {
+        clearCanvas();
+        currentYear = Number(slider.value);
+        updateUI(currentYear);
+        drawAxis();
+        drawCircles(currentYear);
+        countryNextYear();
+        countryFocus(currentCountry);
+        populateInfo();
+        activateCurrentEntry(currentCountry.name);
+    });
+
+    updateUI(currentYear);
     drawAxis();
-    drawCircles(1979);
+    drawCircles(currentYear);
     populateInfo();
 }
 
-function initUI(year) {
+function updateUI(year) {
     document.getElementById('title').innerText = data.title + " - " + year;
 }
 
-function setDimensions(destination, source, heightDifference, widthDifferene) {
+function setDimensions(destination, source, heightDifference, widthDifference) {
     destination.height = source.offsetHeight - heightDifference;
-    destination.width = source.offsetWidth - widthDifferene;
+    destination.width = source.offsetWidth - widthDifference;
 }
 
 function getMaxValue(parameter) {
@@ -65,6 +93,10 @@ function getMaxValue(parameter) {
         default:
             console.log('param is no good');
     }
+}
+
+function scaleCanvasCoordinates() {
+    context.scale(canvas.width / canvasFullWidth, 1);
 }
 
 function drawAxis() {
@@ -160,9 +192,7 @@ function drawCircle(country) {
 }
 
 function drawCircles(year) {
-
     maxRound = Math.round(getMaxValue("round") + 0.1 * getMaxValue("round"));
-
     let searchedYear = data.years.find(currentYear => currentYear.year === year);
 
     if (!searchedYear) {
@@ -182,8 +212,11 @@ function getCircleSize(value) {
 }
 
 function populateInfo() {
-
     let container = document.querySelector('.info');
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 
     let fragment = document.createDocumentFragment();
 
@@ -223,7 +256,7 @@ function createInfoEntry(country) {
 }
 
 function clearCanvas() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvasFullWidth, canvas.height);
     context.strokeStyle = "black";
     context.fillStyle = "black";
     context.setLineDash([]);
@@ -232,10 +265,21 @@ function clearCanvas() {
 function countryFocus(country) {
     clearCanvas();
     drawAxis();
-    drawCircles(1979);
+    drawCircles(currentYear);
+
+    if (country === null) {
+        return;
+    }
+
+    currentCountry = country;
 
     let yDistance = origin.y - (country.firstParam / yValueDistance * yMarkDistance);
     let xDistance = origin.x + (country.secondParam / xValueDistance * xMarkDistance);
+
+    context.fillStyle = country.color;
+    context.beginPath();
+    context.arc(xDistance, yDistance, getCircleSize(country.thirdParam), 0, 2 * Math.PI);
+    context.fill();
 
     context.fillStyle = "black";
     context.beginPath();
@@ -264,10 +308,56 @@ function countryFocus(country) {
     context.fillText(country.thirdParam + " " + data.thirdParam, xDistance + 15, yDistance);
     context.fillText(country.firstParam, xDistance / 2, yDistance - 5);
     context.fillText(country.secondParam, xDistance + 5, yDistance + yDistance / 2);
+
+    resetActiveEntry();
+    activateCurrentEntry(country.name);
 }
 
 function resetCountryFocus() {
+    currentCountry = null;
     clearCanvas();
     drawAxis();
-    drawCircles(1979);
+    drawCircles(currentYear);
+    resetActiveEntry();
+}
+
+function resetActiveEntry() {
+    let activeEntry = document.querySelector('.info-entry-active');
+
+    if (activeEntry != null) {
+        activeEntry.classList.remove("info-entry-active");
+    }
+}
+
+function activateCurrentEntry(countryName) {
+    let entries = document.getElementsByClassName('info-entry');
+    for (let i = 0; i < entries.length; i++) {
+        if (entries[i].firstElementChild.innerText === countryName) {
+            entries[i].classList.add('info-entry-active');
+            return;
+        }
+    }
+}
+
+function calculateYearRange() {
+    minYear = data.years[0].year;
+    maxYear = data.years[data.years.length - 1].year;
+}
+
+function countryNextYear() {
+
+    if (currentCountry === null) {
+        return;
+    }
+
+    data.years.map(year => {
+        if (year.year === currentYear) {
+            year.countries.map(country => {
+                if (country.name === currentCountry.name) {
+                    currentCountry = country;
+                    return;
+                }
+            })
+        }
+    })
 }
